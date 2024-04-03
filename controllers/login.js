c51ddeff3b422c6d203d7f4cdcde2b8853db99aa
alexgoni <goni000211@gmail.com>
2024-04-03 23:06:58 +0900

    refactor: 유효성 검사 순차적으로 진행되도록 변경

--- controllers/login.js
+++ controllers/login.js
@@ -3,60 +3,58 @@ import { isEmpty, isValidEmail, isValidPassword } from "../modules/auth.js";
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const submitBtn = document.querySelector(".form__submit-btn");
+
 const loginErrorMsg = document.createElement("span");
-const passwordErrorMsg = document.createElement("span");
 loginErrorMsg.classList.add("error-msg");
+emailInput.after(loginErrorMsg);
+const passwordErrorMsg = document.createElement("span");
 passwordErrorMsg.classList.add("error-msg");
+passwordInput.after(passwordErrorMsg);
 
 emailInput.addEventListener("focusout", (e) => {
   const input = e.currentTarget;
   const { value } = input;
-  const errorCondition = isEmpty(value) || !isValidEmail(value);
 
-  if (errorCondition) {
+  if (isEmpty(value)) {
     input.classList.add("error");
-    loginErrorMsg.textContent = isEmpty(value)
-      ? "이메일을 입력해주세요"
-      : "잘못된 이메일입니다";
-    input.after(loginErrorMsg);
+    loginErrorMsg.textContent = "이메일을 입력해주세요";
+  } else if (!isValidEmail(value)) {
+    input.classList.add("error");
+    loginErrorMsg.textContent = "잘못된 이메일입니다";
   } else {
     input.classList.remove("error");
-    loginErrorMsg.remove();
+    loginErrorMsg.textContent = "";
   }
 });
 
 passwordInput.addEventListener("focusout", (e) => {
   const input = e.currentTarget;
   const { value } = input;
-  const errorCondition = isEmpty(value) || !isValidPassword(value);
 
-  if (errorCondition) {
+  if (isEmpty(value)) {
     input.classList.add("error");
-    passwordErrorMsg.textContent = isEmpty(value)
-      ? "비밀번호를 입력해주세요"
-      : "비밀번호를 8자 이상 입력해주세요";
-    input.after(passwordErrorMsg);
+    passwordErrorMsg.textContent = "비밀번호를 입력해주세요";
+  } else if (!isValidPassword(value)) {
+    input.classList.add("error");
+    passwordErrorMsg.textContent = "비밀번호를 8자 이상 입력해주세요";
   } else {
     input.classList.remove("error");
-    passwordErrorMsg.remove();
+    passwordErrorMsg.textContent = "";
   }
 });
 
 function updateSignUpButton() {
-  const validFormConditions =
-    !isEmpty(emailInput.value) &&
-    isValidEmail(emailInput.value) &&
-    !isEmpty(passwordInput.value) &&
-    isValidPassword(passwordInput.value);
+  const emailValid =
+    !isEmpty(emailInput.value) && isValidEmail(emailInput.value);
+  const passwordValid =
+    !isEmpty(passwordInput.value) && isValidPassword(passwordInput.value);
 
-  if (validFormConditions) submitBtn.disabled = false;
-  else submitBtn.disabled = true;
+  submitBtn.disabled = !(emailValid && passwordValid);
 }
 
 updateSignUpButton();
 emailInput.addEventListener("input", updateSignUpButton);
 passwordInput.addEventListener("input", updateSignUpButton);
-// TODO: input / change 비교
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
