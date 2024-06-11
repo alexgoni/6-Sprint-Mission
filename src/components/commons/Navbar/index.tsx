e21f366fa9bfb0e73090cb92f1c9257f85074b2e
alexgoni <goni000211@gmail.com>
2024-06-11 10:57:06 +0900

    feat: Navbar 활성화

--- src/components/commons/Navbar/index.tsx
+++ src/components/commons/Navbar/index.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import classNames from "classnames";
 import { LinkButton } from "@/components/commons/Button";
 import useDeviceState from "@/hooks/useDeviceState";
 import Device from "@/variables/Device";
+import getCookie from "@/libs/cookie";
 
 export default function Navbar() {
   const router = useRouter();
   const { pathname } = router;
   const deviceState = useDeviceState();
+
+  const boardsLinkClassnames = classNames({
+    "text-main-blue":
+      pathname.includes("/boards") || pathname.includes("/addboard"),
+  });
+
+  const itemsLinkClassnames = classNames({
+    "text-main-blue": pathname.includes("/items"),
+  });
 
   return (
     <nav className="sticky top-0 z-nav flex h-[70px] items-center border-b-[1px] border-b-[#dfdfdf] bg-white px-4 py-[10px] md:px-[34px] xl:px-[200px]">
@@ -33,21 +44,25 @@ export default function Navbar() {
       </Link>
 
       <div className="ml-[15px] flex flex-grow items-center gap-2 font-bold text-[#4b5563] md:ml-8 md:gap-[38px] md:text-lg">
-        <Link
-          href="/boards"
-          className={`${pathname === "/boards" && "text-main-blue"}`}
-        >
+        <Link href="/boards" className={boardsLinkClassnames}>
           자유게시판
         </Link>
-        <Link
-          href="/items"
-          className={`${pathname === "/items" && "text-main-blue"}`}
-        >
+        <Link href="/items" className={itemsLinkClassnames}>
           중고마켓
         </Link>
       </div>
 
-      <LinkButton href="/login">로그인</LinkButton>
+      {getCookie("accessToken") ? (
+        <Image
+          src="/images/profile.svg"
+          alt="profile"
+          width={40}
+          height={40}
+          className="size-8 rounded-full object-cover"
+        />
+      ) : (
+        <LinkButton href="/login">로그인</LinkButton>
+      )}
     </nav>
   );
 }
